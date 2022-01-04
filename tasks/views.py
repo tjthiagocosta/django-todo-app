6f3bb468d7c1efa7cd7300c5d260a98e39d80e96
Thiago Costa <86612543+tjthiagocosta@users.noreply.github.com>
2022-01-03 21:47:51 -0300

    add feature newtask

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import *
@@ -8,6 +8,13 @@ from .forms import *
 def index(request):
     tasks = Task.objects.all()
 
-    form = TaskForm()   
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
     context = {'tasks': tasks, 'form': form}
     return render(request, 'tasks/list.html', context)
