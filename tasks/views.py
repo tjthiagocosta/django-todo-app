9e604404102bdc82150f86f517119e09d3b8fd90
Thiago Costa <86612543+tjthiagocosta@users.noreply.github.com>
2022-01-04 22:13:22 -0300

    add feature edit task

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
@@ -18,3 +19,22 @@ def index(request):
 
     context = {'tasks': tasks, 'form': form}
     return render(request, 'tasks/list.html', context)
+
+
+def update_task(request, pk):
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+
+        if form.is_valid():
+            form.save()
+
+        return redirect('/')
+
+
+    context = {"form": form}
+
+    return render(request, 'tasks/update_task.html', context)
